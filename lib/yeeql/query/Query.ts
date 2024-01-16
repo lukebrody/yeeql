8489761cc175565e571860ebe5064fbfa7851e92
Luke Brody <lukebrody@pavonine.co>
2024-01-15 16:09:33 -0800

    Add comment, remove unused extractor

--- lib/yeeql/query/Query.ts
+++ lib/yeeql/query/Query.ts
@@ -1,3 +1,7 @@
+/**
+ * The `PrimitiveResult` type is used when using the query's results in a `sort` function.
+ * It should only include primitive values, e.g. values where yjs will inform yeeql when they change.
+ */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export interface Query<Result, Change, PrimitiveResult> {
 	readonly result: Result
@@ -10,6 +14,3 @@ export type QueryResult<Q extends Query<unknown, unknown, unknown>> =
 
 export type QueryChange<Q extends Query<unknown, unknown, unknown>> =
 	Q extends Query<unknown, infer Change, unknown> ? Change : never
-
-export type QueryPrimitiveResult<Q extends Query<unknown, unknown, unknown>> =
-	Q extends Query<unknown, unknown, infer Result> ? Result : never
